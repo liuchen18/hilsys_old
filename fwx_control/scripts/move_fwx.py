3c9b338f8440c3f121b8baccc71dfc6598301c6a
liuchen18 <chen_liu14@163.com>
2020-04-21 09:20:34 -0400

    rewrite the move_fwx.py

--- fwx_control/scripts/move_fwx.py
+++ fwx_control/scripts/move_fwx.py
@@ -15,6 +15,25 @@ class fwx_control_class():
         self.joint6_pub=rospy.Publisher('joint6_command',Float64,queue_size=10)
         self.joint7_pub=rospy.Publisher('joint7_command',Float64,queue_size=10)
         self.base_vel_pub=rospy.Publisher('cmd_vel',Twist,queue_size=10)
+        self.time,self.joint1,self.joint2,self.joint3,self.joint4,self.joint5,self.joint6,self.joint7=[],[],[],[],[],[],[],[]
+        self.x,self.y,self.theta=[],[],[]
+        with open('/home/chen/ws_chen/src/hilsys/sim_sys/data/fwx_planned_trajectory.txt','r') as f:
+            for line in f.readlines()[1:]:
+                data=list(map(float,line.split()))
+                self.time.append(copy.deepcopy(data[0]))
+                self.x.append(copy.deepcopy(data[1]))
+                self.y.append(copy.deepcopy(data[2]))
+                self.theta.append(copy.deepcopy(data[3]))
+                self.joint1.append(copy.deepcopy(data[4]))
+                self.joint2.append(copy.deepcopy(data[5]))
+                self.joint3.append(copy.deepcopy(data[6]))
+                self.joint4.append(copy.deepcopy(data[7]))
+                self.joint5.append(copy.deepcopy(data[8]))
+                self.joint6.append(copy.deepcopy(data[9]))
+                self.joint7.append(copy.deepcopy(data[10]))
+        rospy.loginfo('got the planned data')
+        self.time_duration=self.time[2]-self.time[1]
+        self.rate=rospy.Rate(1/self.time_duration)
         
     def iiwa_msg_pub(self,joint1_v,joint2_v,joint3_v,joint4_v,joint5_v,joint6_v,joint7_v):
         '''
@@ -46,55 +65,42 @@ class fwx_control_class():
         msg.angular.z=theta
         self.base_vel_pub.publish(msg)
 
+    def iiwa_init(self):
+        temp_count=0
+        while temp_count < 8:
+            self.iiwa_msg_pub(self.joint1[0]/8/self.time_duration,self.joint2[0]/8/self.time_duration,self.joint3[0]/8/self.time_duration,self.joint4[0]/8/self.time_duration,self.joint5[0]/8/self.time_duration,self.joint6[0]/8/self.time_duration,self.joint7[0]/8/self.time_duration,)
+            temp_count+=1
+            self.rate.sleep()
+    
+    def compute_velocity(self,time_count):
+        joint1_vel=(self.joint1[time_count+1]-self.joint1[time_count])/self.time_duration
+        joint2_vel=(self.joint2[time_count+1]-self.joint2[time_count])/self.time_duration
+        joint3_vel=(self.joint3[time_count+1]-self.joint3[time_count])/self.time_duration
+        joint4_vel=(self.joint4[time_count+1]-self.joint4[time_count])/self.time_duration
+        joint5_vel=(self.joint5[time_count+1]-self.joint5[time_count])/self.time_duration
+        joint6_vel=(self.joint6[time_count+1]-self.joint6[time_count])/self.time_duration
+        joint7_vel=(self.joint7[time_count+1]-self.joint7[time_count])/self.time_duration
+        x_v=(self.x[time_count+1]-self.x[time_count])/self.time_duration
+        y_v=(self.y[time_count+1]-self.y[time_count])/self.time_duration
+        print(y_v)
+        theta_v=(self.theta[time_count+1]-self.theta[time_count])/self.time_duration
+        return joint1_vel,joint2_vel,joint3_vel,joint4_vel,joint5_vel,joint6_vel,joint7_vel,x_v,y_v,theta_v
+
 
         
 
 def main():
     rospy.init_node('fwx_move')
     fwx_control=fwx_control_class()
-    time,joint1,joint2,joint3,joint4,joint5,joint6,joint7=[],[],[],[],[],[],[],[]
-    x,y,theta=[],[],[]
-    with open('/home/chen/ws_chen/src/hilsys/sim_sys/data/fwx_planned_trajectory.txt','r') as f:
-        for line in f.readlines()[1:]:
-            data=list(map(float,line.split()))
-            time.append(copy.deepcopy(data[0]))
-            x.append(copy.deepcopy(data[1]))
-            y.append(copy.deepcopy(data[2]))
-            theta.append(copy.deepcopy(data[3]))
-            joint1.append(copy.deepcopy(data[4]))
-            joint2.append(copy.deepcopy(data[5]))
-            joint3.append(copy.deepcopy(data[6]))
-            joint4.append(copy.deepcopy(data[7]))
-            joint5.append(copy.deepcopy(data[8]))
-            joint6.append(copy.deepcopy(data[9]))
-            joint7.append(copy.deepcopy(data[10]))
-    rospy.loginfo('got the planned data')
-    time_duration=time[2]-time[1]
     while rospy.get_time() == 0:
         continue
-    rate=rospy.Rate(1/time_duration)
-    temp_count=0
-    while temp_count < 8:
-        fwx_control.iiwa_msg_pub(joint1[0]/8/time_duration,joint2[0]/8/time_duration,joint3[0]/8/time_duration,joint4[0]/8/time_duration,joint5[0]/8/time_duration,joint6[0]/8/time_duration,joint7[0]/8/time_duration,)
-        temp_count+=1
-        rate.sleep()
-    rospy.sleep(1)
+    fwx_control.iiwa_init()
     start_time=rospy.get_time()
-    msg_length=len(time)
+    msg_length=len(fwx_control.time)
     time_count=0
     while not rospy.is_shutdown():
         if time_count<msg_length-1:
-            joint1_vel=(joint1[time_count+1]-joint1[time_count])/time_duration
-            joint2_vel=(joint2[time_count+1]-joint2[time_count])/time_duration
-            joint3_vel=(joint3[time_count+1]-joint3[time_count])/time_duration
-            joint4_vel=(joint4[time_count+1]-joint4[time_count])/time_duration
-            joint5_vel=(joint5[time_count+1]-joint5[time_count])/time_duration
-            joint6_vel=(joint6[time_count+1]-joint6[time_count])/time_duration
-            joint7_vel=(joint7[time_count+1]-joint7[time_count])/time_duration
-            x_v=(x[time_count+1]-x[time_count])/time_duration
-            y_v=(y[time_count+1]-y[time_count])/time_duration
-            print(y_v)
-            theta_v=(theta[time_count+1]-theta[time_count])/time_duration
+            joint1_vel,joint2_vel,joint3_vel,joint4_vel,joint5_vel,joint6_vel,joint7_vel,x_v,y_v,theta_v=fwx_control.compute_velocity(time_count)
             fwx_control.iiwa_msg_pub(joint1_vel,joint2_vel,joint3_vel,joint4_vel,joint5_vel,joint6_vel,joint7_vel)
             fwx_control.base_msg_pub(x_v,y_v,theta_v)
         else:
@@ -103,7 +109,7 @@ def main():
             rospy.loginfo('the planned trajectory is finished')
             exit()
         time_count+=1
-        rate.sleep()
+        fwx_control.rate.sleep()
 
 
     
